41613fbd6ef73a2c968121741ad1b9f8963fed98
EddieSeven <charlescd7@gmail.com>
2018-06-15 16:36:28 -0600

    CSS Adjustments for Info Panel v1

--- src/InfoPanel.jsx
+++ src/InfoPanel.jsx
@@ -13,41 +13,41 @@ class InfoPanel extends Component {
         return (
             <div className="Info-Panel">
 
-                <h3>American Football</h3>
+                <h2>American Football</h2>
                 <div className="info-container">
                     <div className="quarter-container">
-                        <h4>Quarter</h4>
+                        <h5>Quarter</h5>
                         <p>1</p>
                     </div>
 
                     <div className="timeout-container">
                         <div className="timeouts-home">
-                            <h4>Time Outs:</h4>
+                            <h5>Timeouts:</h5>
                             <p>3</p>
                         </div>
                         <div className="challenges-home">
-                            <h4>Challenges:</h4>
+                            <h5>Challenges:</h5>
                             <p>2</p>
                         </div>
                     </div>
 
-                    <div className="yard-line"><h4>50</h4><p>Yard Line</p></div>
+                    <div className="yard-line"><h1>50</h1><p>YardLine</p></div>
 
                     <div className="timeout-container">
                         <div className="timeouts-home">
-                            <h4>Time Outs:</h4>
+                            <h5>Timeouts:</h5>
                             <p>3</p>
                         </div>
                         <div className="challenges-home">
-                            <h4>Challenges:</h4>
+                            <h5>Challenges:</h5>
                             <p>2</p>
                         </div>
                     </div>
 
                     <div className="downs-container">
-                        <h4>Downs</h4>
-                        <div className="down"><p>3<span>rd Down</span></p></div>
-                        <div className="togo"><p>7<span> yards to go</span></p></div>
+                        <h5>Downs</h5>
+                        <div className="down"><p>3<span>rd &</span></p></div>
+                        <div className="togo"><p>7<span> to go</span></p></div>
                     </div>
                 </div>
                 <div className="possession-picker">Graphic</div>
